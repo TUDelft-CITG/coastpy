e70e04ca9e39ad8e23742f681fa36c332d64e0a7
FlorisCalkoen <floris_calkoen@hotmail.com>
2022-05-22 15:16:18 +0200

    test v1

--- src/coastpy/__init__.py
+++ src/coastpy/__init__.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-# Hacky solution for vscode debugger in modular mode.  
+# Hacky solution for vscode debugger in modular mode.
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 # read version from installed package
-# from importlib.metadata import version
+from importlib.metadata import version
 
-# __version__ = version("coastpy")
-
+__version__ = version("coastpy")
