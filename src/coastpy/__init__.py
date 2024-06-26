92ac5a6a99e253c7f24f1cb7d8ed587be15bea29
floriscalkoen <floris_calkoen@hotmail.com>
2024-06-26 18:54:05 +0200

    bump version to trigger package build

--- src/coastpy/__init__.py
+++ src/coastpy/__init__.py
@@ -2,13 +2,13 @@
 
 __author__ = """Floris Calkoen"""
 __email__ = ""
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
-from . import io  
-from . import geo  
-from . import libs  
-from . import stac  
-from . import stats  
-from . import utils  
+from . import io
+from . import geo
+from . import libs
+from . import stac
+from . import stats
+from . import utils
 
 __all__ = ["io", "geo", "libs", "stac", "stats", "utils"]
