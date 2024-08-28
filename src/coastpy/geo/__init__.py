dd98ba7a2671797aff578fcf06281823cc75dd38
floriscalkoen <floris_calkoen@hotmail.com>
2024-08-28 10:38:36 +0200

    add offset rectangle

--- src/coastpy/geo/__init__.py
+++ src/coastpy/geo/__init__.py
@@ -1,4 +1,5 @@
+from .geoms import create_offset_rectangle
 from .quadtiles import make_mercantiles
 from .quadtiles_utils import add_geo_columns
 
-__all__ = ["add_geo_columns", "make_mercantiles", ""]
+__all__ = ["add_geo_columns", "make_mercantiles", "create_offset_rectangle"]
